83e1dcc3c389a8472a07500a8b1b13f3e35742a9
Dylan-Kerler <dylanphoon@gmail.com>
2021-03-27 13:47:01 +0000

    Add frontend

---
+++ frontend/components/layout/View.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const View = styled.div`
+    width: 100%;
+    max-width: 800px;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    border: 1px solid red;
+`;
---
+++ frontend/components/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export const Home = () => {
+    return (
+        <div>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <div>
+                hello there my name is dylan
+            </div>
+        </div>
+    );
+}
